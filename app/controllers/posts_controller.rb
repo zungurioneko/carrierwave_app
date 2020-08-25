0f0e933a7e5313e5a8698fb94a0fbcc5f6c15445
zungurioneko <zungurioneko@gmail.com>
2020-08-25 21:21:46 +0900

    init~AWSとの紐付け

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.order(id: :asc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update!(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.destroy!
+    redirect_to @post
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :image)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
